Corrected wavelength width of kernel to fix normalization issues

development/convolve_highrez_spectra_to_given_R.py
@@ -1,12 +1,13 @@
 '''
+(Adapted from Kareem's binspec/train_NN/convolve_highrez_spectra_to_given_R.py.)
 This code reads in a batch of very high resolution spectra and degrades them
 to a lower resolution, assuming a Gaussian line-spread function. The main use
 case is that we produce synthetic spectra (from an updated version of the Kurucz 
 line list by default) at R~300,000 and need to convolve them the resolution of 
-APOGEE before training the NN spectral model. Note, the high-res spectra are not
+DEIMOS before training the NN spectral model. Note, the high-res spectra are not
 normalized.
 If we were doing ab-initio fitting, it would be important to use the correct
-(non-Gaussian) LSF from APOGEE, but since we just use the synthetic spectral
+(non-Gaussian) LSF from DEIMOS, but since we just use the synthetic spectral
 model to predict the continuum, this isn't important. 
 For a few hundred ab-initio spectra, this runs on my laptop in a minute or two. 
 I have not included the high-res model spectra that this operates on, as they're 
@@ -21,11 +22,16 @@ import multiprocessing
 import utils
 wavelength_template = utils.load_wavelength_array()
 
+D_PayneDir = '/Users/Nathan/Documents/Berkeley/Chemical_Evolution/DEIMOS/D-Payne/'
+SynthSpectraDir = 'spectra/synth_spectra/'
+FileName = 'metal_poor_solar_spectra.npz'
+
 # restore spectra
-temp = np.load('/path/to/highres_spectra.npz')
+print("Reading in synthetic spectra...")
+temp = np.load(D_PayneDir + SynthSpectraDir + FileName)
 wavelength = temp['wavelength'] # this is the R~300,000 wavelength, *not* our default grid.
-spectra = temp['spectra']
-labels = temp['labels']
+spectra = temp['full_spectra']
+labels = temp['feh']
 temp.close()
 
 def sparsify_matrix(lsf):
@@ -55,10 +61,12 @@ wl_res = 1./inv_wl_res
 wl_range = end_wavelength - start_wavelength
 
 # make convolution grid
+print("Making convolution grid...")
 wavelength_run = wl_res*np.arange(wl_range/wl_res + 1)+ start_wavelength
 
-# determines where we can cut off the convolution kernel. 
-template_width = np.median(np.diff(wavelength_template))
+# determines where we can cut off the convolution kernel.
+# multiply by R_apogee / R_deimos to keep template_width large enough
+template_width = np.median(np.diff(wavelength_template)) * 22500 / 5000
 
 # how many kernel bin to keep
 R_range = int(template_width/wl_res + 0.5)*5
@@ -69,6 +77,7 @@ wavelength_tmp = np.concatenate([np.zeros(R_range), wavelength_run,
 
 # create convolution matrix. Each column is a Gaussian kernel with 
 # different FWHM, with the FWMH equal to  FWHM = lambda/R_res
+print("Creating convolution matrix...")
 conv_matrix = np.zeros((len(wavelength_run), 2*R_range+1))
 for i in range(len(wavelength_run)):
     this_wl = wavelength_tmp[i:(i + 2*R_range + 1)] - wavelength_tmp[i + R_range]
@@ -94,9 +103,13 @@ def convolve_spectrum(c1):
     return f_flux_1D(wavelength_template)
 
 # convolve multiple spectra in parallel
+print("Convolving spectra...")
 pool = multiprocessing.Pool(multiprocessing.cpu_count())
 spectra = pool.map(convolve_spectrum, range(spectra.shape[0]))
 
-# save the convolved spectra and their labels 
-np.savez('/path/to/convolved_synthetic_spectra.npz',
-labels = labels, spectra = spectra, wavelength = wavelength_template)
+# save the convolved spectra and their labels
+print("Saving Convolved spectra to %s" % ('convolved_'+FileName))
+np.savez(D_PayneDir + SynthSpectraDir+'convolved_'+FileName,
+         labels = labels, spectra = spectra, wavelength = wavelength_template)
+
+print("Convolution completed!")